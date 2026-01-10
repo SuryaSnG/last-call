26b347879bb84e9d1e7c4b27462f1735e696190d
SuryaSnG <surya.cocoon@gmail.com>
2026-01-10 15:49:51 +0000

    gateway public health endpoint

--- server/gateway-service/src/main/java/com/last/call/gatewayservice/filter/JwtAuthenticationFilter.java
+++ server/gateway-service/src/main/java/com/last/call/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -18,7 +18,7 @@ public class JwtAuthenticationFilter implements WebFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
     private static final Set<String> PUBLIC_PATHS = Set.of(
-        "/user/register", "/user/login", "/item/categories", "/room/live-auctions", "/room/auction-of-the-day"
+        "/user/register", "/user/login", "/item/categories", "/room/live-auctions", "/room/auction-of-the-day", "/health"
     );
     private static final Set<String> PERMITTED_PATHS = Set.of(
             "/item/last-call-to-register", "/item/search", "/item/search-with-filters"
